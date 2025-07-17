15b2964ecf85bc6f51ee73e192e393687c0beb17
Muhammad Bilal <bilalms816@gmail.com>
2025-07-17 18:47:51 +0500

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import ProfilePage from './pages/ProfilePage'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+
+function App() {
+  return (
+  
+    <div className='bg-[url("./src/assets/bgimage.svg")] bg-contain'>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/profile' element={<ProfilePage/>}/>
+        </Routes>
+    </div>
+  )
+}
+
+export default App
